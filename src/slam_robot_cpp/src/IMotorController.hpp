fb911a6873aed56c55e70582dfd0629e98c6b684
R0rschach02 <schwitzendercoder@outlook.de>
2026-02-22 20:50:40 +0100

    feat: add IMotorController abstract base class with JointAngles struct

--- src/slam_robot_cpp/src/IMotorController.hpp
+++ src/slam_robot_cpp/src/IMotorController.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+//hier kommen nur virtuelle Funktionen rein, damit die Motorcontroller Klasse von anderen Klassen geerbt werden kann, um die Motoren zu steuern
+
 struct JointAngles {
     double pan;
     double tilt;
