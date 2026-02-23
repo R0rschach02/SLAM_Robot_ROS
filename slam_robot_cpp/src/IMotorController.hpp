262668136782d1f6dc03d78e39f7421c322228ac
R0rschach02 <schwitzendercoder@outlook.de>
2026-02-23 21:12:24 +0100

    added class GazeboMotorController

---
+++ slam_robot_cpp/src/IMotorController.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+//hier kommen nur virtuelle Funktionen rein, damit die Motorcontroller Klasse von anderen Klassen geerbt werden kann, um die Motoren zu steuern
+
+struct JointAngles {
+    double pan;
+    double tilt;
+};
+
+class IMotorController {
+public:
+    virtual void setAngles(double pan, double tilt) = 0;
+    virtual JointAngles getCurrentAngles() = 0;
+    virtual ~IMotorController() = default;
+};
